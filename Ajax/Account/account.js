d106bba479e3f5d0908868637926764b8c1b0eb9
SBDDZBESTVBB <114671739+SBDDZBESTVBB@users.noreply.github.com>
2023-02-16 14:15:17 +0700

    asvasv

---
+++ Ajax/Account/account.js
@@ -0,0 +1,223 @@
+function create(img) {
+    let username = document.getElementById("usernameCreate").value;
+    let password = document.getElementById("passwordCreate").value;
+    let fullName = document.getElementById("fullNameCreate").value;
+    let phone = document.getElementById("phoneCreate").value;
+    let address = document.getElementById("addressCreate").value;
+    var img = img;
+
+    let account = {
+        username: username,
+        password: password,
+        fullName: fullName,
+        phone: phone,
+        address: address,
+        img: img,
+    }
+
+    $.ajax({
+        type: "Post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+
+        },
+        url: "http://localhost:8080/register",
+        data: JSON.stringify(account),
+        //xử lý khi thành công
+        success: function (data) {
+            document.getElementById("usernameCreate").value = "";
+            document.getElementById("passwordCreate").value= "";
+            document.getElementById("fullNameCreate").value= "";
+            document.getElementById("phoneCreate").value= "";
+            document.getElementById("addressCreate").value= "";
+            document.getElementById("imgCreate").value= "";
+            alert("Thành công");
+            show();
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+let imgInp = document.getElementById("imgCreate");
+let blah = document.getElementById("blah");
+function showImg() {
+    let file = imgInp.files;
+    blah.src = URL.createObjectURL(file[0])
+}
+
+function checkUsername(username) {
+    $.ajax({
+        type: "Get",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+
+        },
+        url: "http://localhost:8080/checkUsername/" +username,
+        //xử lý khi thành công
+        success: function (data) {
+            console.log(data)
+        },
+        error: function (err) {
+            alert("Tài khoản đã tồn tại!")
+            console.log(err)
+        }
+    })
+}
+
+function login() {
+    let username = $("#username").val()
+    let password = $("#password").val()
+    let account = {
+        username: username,
+        password: password,
+    }
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        type: "POST",
+        url: "http://localhost:8080/login",
+        data: JSON.stringify(account),
+        success: function (data) {
+            console.log(data)
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("userToken", JSON.stringify(data));
+            window.location.href = "../index.html"
+            hienthiten();
+        },
+        error: function () {
+            let n
+            let username = $("#username").val()
+            let password = $("#password").val()
+            if (username === "") {
+                n = '<p style="color: red">**Please enter username!</p>'
+                document.getElementById("usernameNotification").innerHTML = n
+                document.getElementById("passwordNotification").innerHTML = ""
+            } else if (password === "") {
+                n = '<p style="color: red">**Please enter password!</p>'
+                document.getElementById("usernameNotification").innerHTML = ""
+                document.getElementById("passwordNotification").innerHTML = n
+            } else {
+                let n = '<p style="color: red">**Wrong account or password!</p>'
+                document.getElementById("usernameNotification").innerHTML = n
+                document.getElementById("passwordNotification").innerHTML = ""
+            }
+        }
+    })
+    event.preventDefault();
+}
+
+function hienthiten() {
+    if (localStorage.getItem("token") != null) {
+        let account = JSON.parse(localStorage.getItem("userToken"));
+        document.getElementById("username").innerText = account.username ;
+        document.getElementById("idAccount").innerText = account.id ;
+    }
+}
+hienthiten();
+
+function upImg() {
+    let fileImg = document.getElementById("imgCreate").files;
+    var formData = new FormData();
+    formData.append("fileImg", fileImg[0]);
+
+    $.ajax({
+        contentType: false,
+        processData: false,
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        type: "POST",
+        data: formData,
+        url: "http://localhost:8080/user/upImg",
+        success: function (img) {
+            create(img)
+        }
+    });
+}
+
+function showAccountDetails(username) {
+    console.log(username)
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        url: "http://localhost:8080/user/" + username,
+        //xử lý khi thành công
+        success: function (account) {
+            console.log(account)
+            localStorage.setItem("fullNameDetails", account.fullName)
+            localStorage.setItem("addressDetails", account.address)
+            localStorage.setItem("phoneDetails", account.phone)
+            localStorage.setItem("imgDetails", account.img)
+            window.location.href = "../AccountDetail.html"
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+function showEditProfile(username){
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        url: "http://localhost:8080/edit/" + username,
+        //xử lý khi thành công
+        success: function (account) {
+            localStorage.setItem("accountEdit", account);
+            document.getElementById("idEdit").value = account.idAccount;
+            document.getElementById("usernameEdit").value = account.username;
+            document.getElementById("passwordEdit").value = account.password;
+            document.getElementById("fullNameUdate").value  = account.fullName;
+            document.getElementById("addressUpdate").value = account.address;
+            document.getElementById("phoneUpdate").value = account.phone;
+            document.getElementById("imgUpdate").src = account.img;
+            console.log(account)
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function updateAccount() {
+    let account = {
+        "idAccount": document.getElementById("idEdit").value,
+        "username": document.getElementById("usernameEdit").value,
+        "password": document.getElementById("passwordEdit").value,
+        "fullName": document.getElementById("fullNameUdate").value,
+        "address": document.getElementById("addressUpdate").value,
+        "phone": document.getElementById("phoneUpdate").value,
+        "img": document.getElementById("imgUpdate").src,
+    }
+
+    $.ajax({
+        type: "PUT",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: "http://localhost:8080/update",
+        data: JSON.stringify(account),
+        //xử lý khi thành công
+        success: function (data) {
+            alert("Thành công");
+            showAccountDetails()
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
---
+++ Ajax/Product/product.js
@@ -0,0 +1,310 @@
+function show(StringGender) {
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        url: "http://localhost:8080/products/gender/" + StringGender,
+        //xử lý khi thành công
+        success: function (productGender) {
+            console.log(productGender)
+            let str = '';
+            for (const p of productGender.content) {
+                str += `<div class="product-item men">
+                                            <div class="product discount product_filter">
+                                                <div class="product_image">
+                                                    <img src="${p.img}" alt="">
+                                                </div>
+                                                <div class="favorite favorite_left"></div>
+                                                <div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center">
+                                                    <span>-$20</span></div>
+                                                <div class="product_info">
+                                                    <h6 class="product_name"><a onclick="showDetailPage(${p.idProduct})" >${p.nameProduct}</a></h6>
+                                                    <div class="product_price">${p.price}</div>
+                                                </div>
+                                            </div>
+                                             <div class="red_button add_to_cart_button" onclick ="addtocart(${p.idProduct})"> Add to cart </div>
+                                            
+                                        </div>`
+            }
+            document.getElementById("show").innerHTML = str;
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+
+function showCategoryByName(name) {
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        url: "http://localhost:8080/products/category/" + name,
+        //xử lý khi thành công
+        success: function (product) {
+            let str = '';
+            for (const p of product.content) {
+                console.log(p)
+                str += `<div class="product-item men">
+                                            <div class="product discount product_filter">
+                                                <div class="product_image">
+                                                    <img src="${p.img}" alt="">
+                                                </div>
+                                                <div class="favorite favorite_left"></div>
+                                                <div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center">
+                                                    <span>-$20</span></div>
+                                                <div class="product_info">
+                                                    <h6 class="product_name" onclick="showDetailPage(${p.idProduct})" ><a >${p.nameProduct}</a></h6>
+                                                    <div class="product_price">${p.price}</div>
+                                                </div>
+                                           </div>
+                                             <div class="red_button add_to_cart_button" onclick ="addtocart(${p.idProduct})"> Add to cart </div>
+                                            
+                                        </div>`
+            }
+            document.getElementById("show").innerHTML = str;
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function searchByName(stringSearch) {
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        url: "http://localhost:8080/products/search/" + stringSearch,
+        //xử lý khi thành công
+        success: function (productSearch) {
+            console.log(productSearch)
+            let str = '';
+            for (const p of productSearch.content) {
+                console.log(p)
+                str += `<div class="product-item men">
+                                            <div class="product discount product_filter">
+                                                <div class="${p.img}">
+                                                    <img src="images/product_1.png" alt="">
+                                                </div>
+                                                <div class="favorite favorite_left"></div>
+                                                <div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center">
+                                                    <span>-$20</span></div>
+                                                <div class="product_info">
+                                                    <h6 class="product_name"><a  onclick="showDetailPage('${p.idProduct}')" >${p.nameProduct}</a></h6>
+                                                    <div class="product_price">${p.price}</div>
+                                                </div>
+                                            </div>
+                                            </div>
+                                             <div class="red_button add_to_cart_button" onclick ="addtocart(${p.idProduct})"> Add to cart </div>
+                                            
+                                        </div>`
+            }
+            document.getElementById("show").innerHTML = str;
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function showDetailPage(id) {
+    localStorage.setItem("productDetailId", id);
+    window.location.href = "../single.html"
+
+}
+
+function showProductById(id) {
+    alert(id)
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        },
+        url: "http://localhost:8080/products/" + id,
+        //xử lý khi thành công
+        success: function (product) {
+            console.log(product)
+            let str = '';
+            console.log(product)
+            str += `<div class="col-lg-7">
+            <div class="single_product_pics">
+ <div class="row">
+ <div class="col-lg-3 thumbnails_col order-lg-1 order-2">
+ <div class="single_product_thumbnails">
+ </div>
+ </div>
+ <div class="col-lg-9 image_col order-lg-2 order-1">
+ <div class="single_product_image">
+ <div class="single_product_image_background" style="background-image:url('/images/single_2.jpg')"></div>
+ </div>
+ </div>
+ </div>
+</div>
+ </div>
+<div class="col-lg-5">
+ <div class="product_details">
+ <div class="product_details_title">
+ <h2>${product.nameProduct}</h2>
+ </div>
+ <div class="free_delivery d-flex flex-row align-items-center justify-content-center">
+ <span class="ti-truck"></span><span>free delivery</span>
+ </div>
+ <div class="original_price">$629.99</div>
+ <div class="product_price">${product.price}</div>
+ <div class="quantity d-flex flex-column flex-sm-row align-items-sm-center">
+ <span>Quantity:</span>
+ <div class="quantity_selector">
+ <span class="minus"><i class="fa fa-minus" aria-hidden="true"></i></span>
+ <span id="quantity_value">1</span>
+ <span class="plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
+ </div>
+ <div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
+ <div class="product_favorite d-flex flex-column align-items-center justify-content-center"></div>
+ </div>
+ </div>
+ </div>`
+
+            document.getElementById("showProduct").innerHTML = str;
+
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function addtocart(idproduct) {
+    if (localStorage.getItem("userToken") == null) {
+        alert("can dang nhap hoac dang ky")
+    } else {
+        let amount = +prompt("nhap so luong can mua");
+        let account = JSON.parse(localStorage.getItem("userToken"));
+        let username = account.username
+        console.log(idproduct)
+        let cartdetail = {
+            'amount': amount,
+            'product': {
+                'idProduct': idproduct
+            },
+            'account': {
+                'username': username
+            }
+
+        }
+
+        $.ajax({
+            type: "Post",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+
+            },
+            url: "http://localhost:8080/cart/",
+            data: JSON.stringify(cartdetail),
+            //xử lý khi thành công
+            success: function (data) {
+                alert("Thành công");
+
+            },
+            error: function (err) {
+                console.log(err)
+                alert("Can dang nhap hoac dang ky")
+            }
+        })
+    }
+}
+
+function showcart() {
+    let account = JSON.parse(localStorage.getItem("userToken"));
+    let username = account.username;
+    $.ajax({
+        type: "Get",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+
+        },
+        url: "http://localhost:8080/cart/" + username,
+        //xử lý khi thành công
+        success: function (cartdetails) {
+
+            console.log(cartdetails)
+            let str = '';
+            for (const c of cartdetails)
+                str += `
+                <tr>
+                <td>${c.product.nameProduct}</td>
+                <td><img width="300px" src="${c.product.img}"></td>
+                <td>${c.product.price}</td>
+                <td>${c.amount}</td>
+                <td>${c.product.price * c.amount}</td>
+               <td>
+               <button onclick="deletecart(${c.idCartdetail})">Delete</button>
+                </td>
+                </tr>
+                `
+
+            document.getElementById("showcart").innerHTML = str;
+            alert("thanh cong")
+
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+showcart();
+
+function createbill() {
+    let account = JSON.parse(localStorage.getItem("userToken"));
+    let username = account.username;
+    $.ajax({
+        type: "Post",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+
+        },
+        url: "http://localhost:8080/bill/" + username,
+        success: function (data) {
+            alert("Thành công");
+
+        },
+        error: function (err) {
+            console.log(err)
+            alert("Can dang nhap hoac dang ky")
+        }
+    })
+
+}
+function deletecart(idCartdetails){
+    $.ajax({
+        type: "Post",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+
+        },
+        url: "http://localhost:8080/cart/"+idCartdetails,
+        success: function (data) {
+            alert("Thành công");
+
+        },
+        error: function (err) {
+            console.log(err)
+            alert("Can dang nhap hoac dang ky")
+        }
+    })
+
+}
